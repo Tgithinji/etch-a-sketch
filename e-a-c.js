99e44fe8f4c80335dbef1171ea83a355ff796ce5
Tgithinji <119703130+Tgithinji@users.noreply.github.com>
2023-02-03 02:23:54 +0300

    Add feature to take user input and create a custom grid

--- e-a-c.js
+++ e-a-c.js
@@ -1,7 +1,11 @@
 const container  = document.querySelector('#container');
+const customBtn = document.querySelector('.prompt');
 
-function makeGrid() {
-    for (let i = 0; i < (16*16); i++) {
+// function to create a number of divs
+function makeGrid(cols) {
+    container.setAttribute('style', `grid-template-rows: repeat(${cols}, 2fr); 
+    grid-template-columns: repeat(${cols}, 2fr);`);
+    for (let i = 0; i < (cols * cols); i++) {
         const div = document.createElement('div');
         container.appendChild(div);
         div.addEventListener('mouseover', () => {
@@ -10,4 +14,24 @@ function makeGrid() {
     }
 }
 
-window.onload = makeGrid;
+// function to delete previous div before creating a new grid
+function removeGrid() {
+    while (container.hasChildNodes()) {
+        container.removeChild(container.firstChild);
+    }
+}
+
+// function that takes a user choice and creates custom grid
+function customGrid() {
+    let cols;
+    do {
+        cols = parseInt(prompt('Enter a number between 1-100', ''))
+    } while (cols < 1 || cols > 100 || (isNaN(cols)));
+    removeGrid();  
+    makeGrid(cols);
+}
+
+customBtn.addEventListener('click', customGrid);
+window.onload = () => {
+    makeGrid(16);
+};
